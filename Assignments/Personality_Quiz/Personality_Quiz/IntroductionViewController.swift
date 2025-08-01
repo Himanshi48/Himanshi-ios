9b5370e56d74f38abed98e21e66f850722567e59
Himanshi48 <156739655+Himanshi48@users.noreply.github.com>
2025-08-01 13:32:59 +0530

    Uploading Personality Quiz Assignment

---
+++ Assignments/Personality_Quiz/Personality_Quiz/IntroductionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Personality_Quiz
+//
+//  Created by Guest1 on 29/07/25.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue){
+        
+    }
+}
+
