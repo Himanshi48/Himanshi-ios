43bf2bd501a32341f2b056823153fa9640b75e85
Himanshi48 <156739655+Himanshi48@users.noreply.github.com>
2025-08-26 07:41:30 +0530

    Adding lab of Table Views - Meal Tracker

---
+++ Assignments/lab-mealtracker/lab-mealtracker/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  lab-mealtracker
+//
+//  Created by Himanshi Godara on 23/08/25.
+//
+
+import Foundation
+
+
+struct Food {
+    var name: String
+    var description: String
+}
---
+++ Assignments/lab-mealtracker/lab-mealtracker/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  lab-mealtracker
+//
+//  Created by Himanshi Godara on 23/08/25.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
