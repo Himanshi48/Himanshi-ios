20f122554b4d13b8824f7549bc3878bff37552b7
Himanshi48 <156739655+Himanshi48@users.noreply.github.com>
2025-08-29 12:45:17 +0530

    Uploading Hotel Codable lab

---
+++ HotelCodable/HotelCodable/hotelDetail.swift
@@ -0,0 +1,42 @@
+//
+//  hotelDetail.swift
+//  HotelCodable
+//
+//  Created by Himanshi Godara on 27/08/25.
+//
+
+import Foundation
+
+struct Registeration{
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    var checkOutDate: Date
+    var checkInDate: Date
+    var numberOfAdult: Int
+    var numberOfChildren: Int
+    var wifi: Bool
+    var roomType: RoomType
+}
+
+struct RoomType: Equatable{
+    var id: Int
+    var name: String
+    var shortName: String
+    var price: Int
+    static func == (lhs: RoomType, rhs: RoomType) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+
+
+extension RoomType {
+    static var all: [RoomType] {
+        return [
+            RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+            RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+            RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)
+        ]
+    }
+}
