1efb1d83a4916601c14b254f36f85dfe75b42e2d
Himanshi48 <156739655+Himanshi48@users.noreply.github.com>
2025-10-21 18:51:00 +0530

    uploading collection view lab

---
+++ 1 - Collection Views/lab/EmojiDictionary/EmojiDictionary/AddEditEmojiTableViewController.swift
@@ -0,0 +1,71 @@
+import UIKit
+
+class AddEditEmojiTableViewController: UITableViewController {
+    
+    var emoji: Emoji?
+
+    @IBOutlet var symbolTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var usageTextField: UITextField!
+    @IBOutlet var saveButton: UIBarButtonItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let emoji = emoji {
+            symbolTextField.text = emoji.symbol
+            nameTextField.text = emoji.name
+            descriptionTextField.text = emoji.description
+            usageTextField.text = emoji.usage
+            title = "Edit Emoji"
+        } else {
+            title = "Add Emoji"
+        }
+        
+        updateSaveButtonState()
+    }
+    
+    init?(coder: NSCoder, emoji: Emoji?) {
+        self.emoji = emoji
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateSaveButtonState() {
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        let usageText = usageTextField.text ?? ""
+        saveButton.isEnabled = containsSingleEmoji(symbolTextField) && !nameText.isEmpty && !descriptionText.isEmpty && !usageText.isEmpty
+    }
+    
+    @IBAction func textEditingChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    func containsSingleEmoji(_ textField: UITextField) -> Bool {
+        guard let text = textField.text, !text.isEmpty else {
+            return false
+        }
+        
+        // This allows single visible emoji (even if it's multiple Unicode scalars)
+        let isSingleEmoji = text.count == 1 ||
+            (text.unicodeScalars.count > 1 && text.unicodeScalars.allSatisfy { $0.properties.isEmojiPresentation || $0.properties.isEmoji })
+        
+        return isSingleEmoji
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "saveUnwind" else { return }
+
+        let symbol = symbolTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        let usage = usageTextField.text ?? ""
+        emoji = Emoji(symbol: symbol, name: name, description: description, usage: usage)
+    }
+}
